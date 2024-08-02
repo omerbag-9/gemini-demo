da021870071fb7a38243d671da05c7895f613b62
omerbag-9 <omerbagprog@gmail.com>
2024-08-02 04:31:32 +0300

    feat players , clubs , category (models , get api's)

--- app/index.js
+++ app/index.js
@@ -3,6 +3,9 @@ import express from 'express'
 import path from 'path'
 import dotenv from 'dotenv'
 import { connectDB } from './db/connection.js'
+import { clubRouter } from './src/modules/club/club.router.js'
+import { categoryRouter } from './src/modules/category/category.router.js'
+import { playerRouter } from './src/modules/player/player.router.js'
 
 // config the .env file
 
@@ -17,6 +20,11 @@ const port = process.env.PORT
 
 app.use(express.json())
 
+// routers
+
+app.use('/club',clubRouter)
+app.use('/category',categoryRouter)
+app.use('/player',playerRouter)
 
 // connect db
 
